86afadb20192ffa8be1b1f550cc1fdbd031ae7c2
Gabriel Chidziya <gabrielgodian61@gmail.com>
2026-02-06 08:08:10 +0200

    Refactor app.py for improved car dealership flow

--- app.py
+++ app.py
@@ -37,7 +37,6 @@ def show_cars():
     cars = {
         "Porsche Cayenne 2026": [250000, "https://images.unsplash.com/photo-1503376780353-7e6692767b70?auto=format&fit=crop&q=80&w=800"],
         "Audi RSQ8 2026": [200000, "https://images.unsplash.com/photo-1606152421802-db97b9c7a11b?auto=format&fit=crop&q=80&w=800"],
-        "Audi RS7 2026": [180000, "https://images.unsplash.com/photo-1614164185128-e4ec99c436d7?auto=format&fit=crop&q=80&w=800"],
         "Mercedes Benz GLE Coupe AMG 2026": [150000, "https://images.unsplash.com/photo-1618843479313-40f8afb4b4d8?auto=format&fit=crop&q=80&w=800"],
         "Mercedes Benz E63 AMG 2026": [190000, "https://images.unsplash.com/photo-1622199024103-60589a194916?auto=format&fit=crop&q=80&w=800"]
     }
@@ -93,4 +92,5 @@ if st.session_state.step == "registration":
 elif st.session_state.step == "showroom":
     show_cars()
 elif st.session_state.step == "payment":
+
     payment()
